5631302b1d31356b038db2672b9bfd4592b3f1f4
viktorderkach777 <viktorderkach777@yandex.ua>
2019-12-05 00:41:48 +0200

    changing selenoid location

--- FluxDayAutomation/FluxDayAutomation/Drivers/SelenoidDrivers.cs
+++ FluxDayAutomation/FluxDayAutomation/Drivers/SelenoidDrivers.cs
@@ -9,14 +9,14 @@ namespace FluxDayAutomation.Drivers
     {
         public const string CHROME = "chrome";
         public const string FIREFOX = "firefox";
-        public const string CHROME_V1 = "76.0";
+        public const string CHROME_V1 = "78.0";
         public const string CHROME_V2 = "77.0";
-        public const string FIREFOX_V1 = "68.0";
+        public const string FIREFOX_V1 = "70.0";
         public const string FIREFOX_V2 = "69.0";
         private const bool ENABLE_VNC = true;
         private const int BROWSER_WIDTH = 1920;
         private const int BROWSER_HEIGHT = 1080;
-        private const string URI = "http://34.70.15.20:4444/wd/hub";
+        private const string URI = "http://104.155.183.244:4444/wd/hub";
 
         static public IWebDriver CreateDriver(string browser, string version)
         {
